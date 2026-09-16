8bd878456e0bb96f4dbf21c20e1c879cc1f5ebe5
Izzat <izzatismadi@gmail.com>
2026-09-16 19:57:21 +0800

    Architectural Decision: group application logic into their respective hooks

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+
+import { getProducts, searchProducts } from "../api/productsApi";
+
+import type { Product } from "../types/product";
+
+import { useDebounce } from "./useDebounce";
+
+const PAGE_SIZE = 20;
+const DEBOUNCE_DELAY = 200;
+
+export function useProducts() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [skip, setSkip] = useState(0);
+    const [search, setSearch] = useState('');
+
+    const debounceSearch = useDebounce({
+        value: search,
+        delay: DEBOUNCE_DELAY
+    })
+    
+    async function loadMoreProducts() {
+        const nextSkip = skip + PAGE_SIZE;
+
+        // load more products based on search value
+        if (debounceSearch.trim()) {
+            const data = await searchProducts({
+                query: debounceSearch,
+                limit: PAGE_SIZE,
+                skip: nextSkip
+            })
+
+            setProducts((currentProducts) => {
+                // merge current data with new data
+                return [
+                    ...currentProducts,
+                    ...data.products
+                ]
+            })
+
+            setSkip(nextSkip)
+            return;
+        }
+
+        const data = await getProducts({
+            limit: PAGE_SIZE,
+            skip: nextSkip
+        })
+
+        setProducts((currentProducts) => {
+            // merge current data with new data
+            return [
+                ...currentProducts,
+                ...data.products
+            ]
+        })
+
+        setSkip(nextSkip);
+    }
+
+    // fetch product based on search value
+    useEffect(() => {
+        // ensure there's no leading space
+        if (!debounceSearch.trim()) {
+            getProducts({
+                limit: PAGE_SIZE,
+                skip: 0
+            }).then((data) => {
+                setProducts(data.products);
+                setSkip(0);
+            })
+
+            return;
+        }
+
+        searchProducts({
+            query: debounceSearch,
+            limit: PAGE_SIZE,
+            skip: 0
+        }).then((data) => {
+            setProducts(data.products);
+            setSkip(0);
+        })
+    }, [debounceSearch])
+
+    return {
+        // state
+        products,
+        search,
+
+        // methods
+        setSearch,
+        loadMoreProducts
+    };
+}
--- src/screens/ProductListScreen.tsx
+++ src/screens/ProductListScreen.tsx
@@ -1,102 +1,18 @@
 import { View, FlatList, StyleSheet } from 'react-native';
-import { useEffect, useState } from 'react';
-import { getProducts, searchProducts } from '../api/productsApi';
-
-import type { 
-    Product 
-} from '../types/product';
 
 import ProductCard from '../components/ProductCard';
 import ProductSearch from '../components/ProductSearch';
 
-import { useDebounce } from '../hooks/useDebounce';
-
-const PAGE_SIZE = 20; // make default limit of 20 data for one pagination
+import { useProducts } from '../hooks/useProducts';
 
 export default function ProductListScreen() {
-    const [products, setProducts] = useState<Product[]>([]);
-    const [skip, setSkip] = useState(0);
-    const [search, setSearch] = useState('');
+    const { 
+        products,
+        search,
 
-    const debounceSearch = useDebounce({ 
-        value: search,
-        delay: 200
-    })
-
-    async function loadMoreProducts() {
-        const nextSkip = skip + PAGE_SIZE;
-
-        // load more products based on search value
-        if (debounceSearch.trim()) {
-            const data = await searchProducts({
-                query: debounceSearch,
-                limit: PAGE_SIZE,
-                skip: nextSkip
-            })
-
-            setProducts((currentProducts) => {
-                // merge current data with new data
-                return [
-                    ...currentProducts,
-                    ...data.products
-                ]
-            })
-
-            setSkip(nextSkip)
-            return;
-        }
-
-        const data = await getProducts({
-            limit: PAGE_SIZE,
-            skip: nextSkip
-        })
-
-        setProducts((currentProducts) => {
-            // merge current data with new data
-            return [
-                ...currentProducts,
-                ...data.products
-            ]
-        })
-
-        setSkip(nextSkip);
-    }
-
-    // fetch products on load
-    useEffect(() => {
-        getProducts({
-            limit: PAGE_SIZE,
-            skip: 0
-        }).then((data) => {
-            setProducts(data.products)
-        })
-    }, [])
-
-    // fetch product based on search value
-    useEffect(() => {
-        // ensure there's no leading space
-        if (!debounceSearch.trim()) {
-            getProducts({
-                limit: PAGE_SIZE,
-                skip: 0
-            }).then((data) => {
-                setProducts(data.products);
-                setSkip(0);
-            })
-            
-            return;
-        }
-
-        searchProducts({
-            query: debounceSearch,
-            limit: PAGE_SIZE,
-            skip: 0
-        }).then((data) => {
-            setProducts(data.products);
-            setSkip(0);
-        })
-        
-    }, [debounceSearch])
+        setSearch,
+        loadMoreProducts,
+    } = useProducts()
 
     return (
         <View style={styles.container}>
